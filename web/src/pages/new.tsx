fd6aa636cbe3e553feaaa446cddeadd1f5bab165
nomercy360 <maximkadocnikov@gmail.com>
2024-12-19 22:25:43 +0700

    missing pages

---
+++ web/src/pages/new.tsx
@@ -0,0 +1,11 @@
+const NewItem = () => {
+    return (
+        <div
+            class="relative w-full flex flex-col h-screen overflow-hidden"
+        >
+            New Item
+        </div>
+    )
+}
+
+export default NewItem

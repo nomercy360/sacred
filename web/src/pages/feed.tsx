420c0d07376e1cef8d7c450b5eb128ad2478f0d4
nomercy360 <maximkadocnikov@gmail.com>
2025-02-03 19:19:47 +0530

    fix search

--- web/src/pages/feed.tsx
+++ web/src/pages/feed.tsx
@@ -3,6 +3,7 @@ import { fetchFeed, Wish } from '~/lib/api'
 import { Link } from '~/components/link'
 import { WishesGrid } from '~/pages/bookmarks'
 import { createSignal, Show } from 'solid-js'
+import { cn } from '~/lib/utils'
 
 const FeedPage = () => {
 	const [search, setSearch] = createSignal('')
@@ -23,43 +24,39 @@ const FeedPage = () => {
 
 	return (
 		<div class="relative flex flex-col items-center w-full h-screen overflow-hidden">
-			<Show when={searchMode()}>
-				<div
-					class="w-full h-20 pt-3 px-5 pb-5">
-					<div class="flex w-full rounded-2xl bg-secondary h-12 flex-row items-center justify-between pl-3">
-						<input
-							ref={setSearchInput}
-							type="text"
-							value={search()}
-							onInput={(e) => setSearch(e.target.value)}
-							class="h-full w-full bg-transparent focus:outline-none"
-							placeholder="Search ideas"
-						/>
-						<button
-							class="bg-secondary rounded-full size-12 flex items-center justify-center"
-							onClick={search() ? () => setSearch('') : toggleSearchMode}
-						>
-							<span class="material-symbols-rounded text-[20px]">close</span>
-						</button>
-					</div>
-				</div>
-			</Show>
-			<Show when={!searchMode()}>
-				<div
-					class="bg-background h-20 flex-shrink-0 w-full flex flex-row justify-between items-center pb-9 pt-5 px-5">
+			<div
+				class={cn('w-full h-20 pt-3 px-5 pb-7', searchMode() ? 'block' : 'hidden')}>
+				<div class="flex w-full rounded-2xl bg-secondary h-10 flex-row items-center justify-between pl-3">
+					<input
+						ref={setSearchInput}
+						type="text"
+						value={search()}
+						onInput={(e) => setSearch(e.target.value)}
+						class="h-full w-full bg-transparent focus:outline-none"
+						placeholder="Search ideas"
+					/>
 					<button
-						class="flex items-center justify-center bg-secondary rounded-full size-10"
-						onClick={toggleSearchMode}
+						class="bg-secondary rounded-full size-10 flex items-center justify-center"
+						onClick={search() ? () => setSearch('') : toggleSearchMode}
 					>
-						<span class="material-symbols-rounded text-[20px]">search</span>
+						<span class="material-symbols-rounded text-[20px]">close</span>
 					</button>
-					<p class="text-black text-xl font-extrabold">Discover</p>
-					<Link href="/categories-edit" state={{ from: '/feed' }}
-								class="flex items-center justify-center bg-secondary rounded-full size-10">
-						<span class="material-symbols-rounded text-[20px]">page_info</span>
-					</Link>
 				</div>
-			</Show>
+			</div>
+			<div
+				class={cn('bg-background h-20 flex-shrink-0 w-full flex flex-row justify-between items-center pb-9 pt-5 px-5', searchMode() ? 'hidden' : 'flex')}>
+				<button
+					class="flex items-center justify-center bg-secondary rounded-full size-10"
+					onClick={toggleSearchMode}
+				>
+					<span class="material-symbols-rounded text-[20px]">search</span>
+				</button>
+				<p class="text-black text-xl font-extrabold">Discover</p>
+				<Link href="/categories-edit" state={{ from: '/feed' }}
+							class="flex items-center justify-center bg-secondary rounded-full size-10">
+					<span class="material-symbols-rounded text-[20px]">page_info</span>
+				</Link>
+			</div>
 			<WishesGrid wishes={wishes as any} source="/feed" />
 		</div>
 	)

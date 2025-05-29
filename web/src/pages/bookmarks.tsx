f94613caec74b2ef32099bae39f54725e40dafb6
dima <dmitriikomarow@gmail.com>
2025-05-29 18:17:17 +0300

    update style

--- web/src/pages/bookmarks.tsx
+++ web/src/pages/bookmarks.tsx
@@ -13,7 +13,7 @@ const BookmarksPage = () => {
 		<div
 			class="relative flex flex-col items-center w-full h-screen overflow-hidden"
 		>
-			<div class="fixed top-0 left-0 right-0 z-10 bg-gradient-to-b from-white to-transparent h-20">
+			<div class="fixed top-0 left-0 right-0 z-10 bg-gradient-to-t from-transparent to-white h-20">
 				<div
 				class="h-20 flex-shrink-0 w-full flex flex-row justify-between items-center pb-9 pt-5 px-5">
 				<button class="flex items-center justify-center bg-secondary rounded-full size-10">
